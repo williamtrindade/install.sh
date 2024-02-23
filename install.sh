69507ded74fe790688f8f4bcb620d18c183882f9
William Trindade <williamtrindade777@gmail.com>
2024-02-22 23:08:54 -0300

    update

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt autoremove
 sudo apt install curl
 
 # SPOTIFY
-curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
 
@@ -25,17 +25,18 @@ sudo apt install php8.1-sqlite3
 
 # COMPOSER
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'edb40769019ccf227279e3bdd1f5b2e9950eb000c3233ee85148944e555d97be3ea4f40c3c2fe73b22f875385f6a5155') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 sudo mv composer.phar /usr/local/bin/composer
 
 # JETBRAINS
-wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.0.2.16660.tar.gz
-tar -xvzf ./jetbrains-toolbox-2.0.2.16660.tar.gz
-sudo rm ./jetbrains-toolbox-2.0.2.16660.tar.gz
-sudo mv ./jetbrains-toolbox-2.0.2.16660 /opt
-/opt/jetbrains-toolbox-2.0.2.16660/jetbrains-toolbox
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.2.1.19765.tar.gz
+
+tar -xvzf ./jetbrains-toolbox-2.2.1.19765.tar.gz
+sudo rm ./jetbrains-toolbox-2.2.1.19765.tar.gz
+sudo mv ./jetbrains-toolbox-2.2.1.19765 /opt
+/opt/jetbrains-toolbox-2.2.1.19765/jetbrains-toolbox
 
 # JAVA
 sudo apt install openjdk-17-jdk -y
@@ -67,10 +68,11 @@ sudo apt-get update
 sudo apt-get install obs-studio -y
 
 
-FLUTTER
-wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.13.1-stable.tar.xz
-tar -xf ./flutter_linux_3.13.1-stable.tar.xz
-sudo rm ./flutter_linux_3.13.1-stable.tar.xz
+# FLUTTER
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.1-stable.tar.xz
+
+tar -xf ./flutter_linux_3.19.1-stable.tar.xz
+sudo rm ./flutter_linux_3.19.1-stable.tar.xz
 sudo mv ./flutter /opt
 export PATH="$PATH:/opt/flutter/bin"
 flutter doctor
